e7f992d22da34a213ad601172f11034b7987fc77
Agam Darmawan <madridlover085@gmail.com>
2025-09-17 20:15:02 +0700

    new materi

--- Array.java
+++ Array.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Masukkan Nama :");
-        String[] nama1 = new String[5];
-        for (int i = 0; i < nama1.length; i++) {
-            nama1[i] = sc.nextLine();
+        double[] nilai = new double[5];
+        double total = 0;
+
+        System.out.println("Masukkan Nilai:");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilai[i] = sc.nextDouble();
+            total += nilai[i];
         }
-        System.out.println("Nama yang dimasukkan : ");
-        for (String nama : nama1) {
-            System.out.println("Nama : " + nama);
-        }
+
+        double rataRata = total / nilai.length;
+        System.out.println("Rata-rata Nilai: " + rataRata);
+
+        sc.close();
     }
 }
